9b736d27eb655e732dc2a5622a0eac473dc9117c
Hai Dang <42664118+pnjdang@users.noreply.github.com>
2022-04-22 15:35:06 +0700

    db

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=192.168.2.131;dbname=hcmgisportal;port=5432',
+    'username' => 'postgres',
+    'password' => '@hcmgis#',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
